08e23bf8aced8497e862b632d9aff47261fd7c9c
iswell100 <welliskandar100@gmail.com>
2023-06-18 06:41:41 +0700

    insert note

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const Hapi = require('@hapi/hapi');
- 
+const routes = require('./routes');
  
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+		routes: {
+			cors: {
+				origin: ['*'], // an array of origins or 'ignore'
+        headers: ["Accept", "Content-Type"],
+        additionalHeaders: ["X-Requested-With"]
+		}
+		}
   });
- 
+
+	server.route(routes);
  
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
